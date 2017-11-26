52d5d00508a3f03361a0d80becbd059809ae89e2
bMik98 <bmik9898@gmail.com>
2017-11-26 12:30:57 +0300

    lombok added

--- src/main/java/ru/aplana/test/dvdxchange/domain/User.java
+++ src/main/java/ru/aplana/test/dvdxchange/domain/User.java
@@ -1,101 +1,73 @@
 package ru.aplana.test.dvdxchange.domain;
 
-import org.hibernate.validator.constraints.Email;
+import lombok.Data;
+import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Описывает сущность ПОЛЬЗОВАТЕЛЬ.
- * Применяется также для аутентификации пользователя.
- * Для пользователя с администраторскими функциями признак supervisor должен быть установлен в true.
+ * Применяется для аутентификации пользователя.
+ * Для пользователя с администраторскими привилегиями признак supervisor должен быть установлен в true.
  * В качестве логина предполагается использовать email.
  *
  * @author bedritskiy_mr
  */
 @Entity
+@Data
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
-    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
-    private Set<Disk> disks;
-
-    @OneToMany(mappedBy = "holder", cascade = CascadeType.ALL)
-    private Set<TakenItem> takenItems;
-
-    @Email
+    @NonNull
+    private String username;
     private String email;
-
-    @Transient
     private String password;
-
-    private boolean supervisor;
-    private String name;
+    private String displayName;
+    private boolean supervisor = false;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private Set<Disk> disks = new HashSet<>();
+    @OneToMany(mappedBy = "holder", cascade = CascadeType.ALL)
+    private Set<TakenItem> takenItems = new HashSet<>();
 
     public User() {
-        supervisor = false;
-        disks = new HashSet<>();
-        takenItems = new HashSet<>();
     }
 
-    public long getId() {
-        return id;
+    public User(final String username) {
+        this.username = username;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return (supervisor) ? Arrays.asList(Role.ADMIN, Role.USER) : Arrays.asList(Role.USER);
     }
 
-    public String getEmail() {
-        return email;
-    }
+    public enum Role implements GrantedAuthority {
+        USER {
+            @Override
+            public String getAuthority() {
+                return "USER";
+            }
+        }, ADMIN {
+            @Override
+            public String getAuthority() {
+                return "ADMIN";
+            }
+        };
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isSupervisor() {
-        return supervisor;
-    }
-
-    public void setSupervisor(boolean supervisor) {
-        this.supervisor = supervisor;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Disk> getDisks() {
-        return disks;
-    }
-
-    public void setDisks(Set<Disk> disks) {
-        this.disks = disks;
-    }
-
-    public Set<TakenItem> getTakenItems() {
-        return takenItems;
-    }
-
-    public void setTakenItems(Set<TakenItem> takenItems) {
-        this.takenItems = takenItems;
+        @Override
+        public abstract String getAuthority();
     }
 }
